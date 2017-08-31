fb555a6106759259cc8b0ba709c2d8a0e6b6f84f
rajesh_4k <4k.rajesh@gmail.com>
2017-08-31 15:59:25 +0530

    logic for the controller updated

---
+++ controllers/stockTickerController.js
@@ -0,0 +1,51 @@
+e strict';
+var app = angular.module('stockApp');
+
+app.controller('stockTickerController',['$scope','stockService',stockTickerController]);
+
+var stockTickerController = function($scope,stockService){
+	var ws = new WebSocket("ws://stocks.mnet.website");
+	var stocksData = {}; 
+	$scope.stocksData=[];
+
+	ws.onopen = (event) => {
+		$scope.message = 'Loading the data ...';
+	}
+
+	ws.onclose = (event) => {
+		$scope.message = 'connection is temporary unavailable';
+	}
+
+	ws.onmessage = 	(data) => {
+		data.forEach(([name, price]) => {
+			stockService.updateData(name, price);
+			updateStocksData(name, price);
+		});
+	}
+
+	function updateStocksData(name, price){
+		if(stocksData[name]){
+			for(var i=0; i<$scope.stocksData.length;i++){
+				if($scope.stocksData[i].name === name){
+					if($scope.stocksData[i]['price'] < price){
+						$scope.stocksData[i]['color'] = 'red';
+					}
+					if($scope.stocksData[i]['price'] > price){
+						$scope.stocksData[i]['color'] = 'green';
+					}
+					$scope.stocksData[i]['price'] = price;
+				}
+			}	
+		}
+		else{
+			stocksData[name] = price;
+			var obj = {
+				'name' : name,
+				'price': price,
+				'color': 'green'
+			}
+			$scope.stocksData.push(obj);
+		}
+	}
+
+}
